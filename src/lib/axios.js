c9ba4fd6be096e11b54c631752179ffdf411127b
Ariel Escobar <arielescobar531@gmail.com>
2026-04-12 00:22:39 -0400

    Fix: hotfix

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
+
 // Se instancia la api base //
 const api = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: apiUrl,
     headers: {
         'Content-Type': 'application/json'
     }
